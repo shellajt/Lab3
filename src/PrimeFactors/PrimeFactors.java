03478b59c81e4d8bcf191b49aa94926f264dbc95
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-03-24 14:55:01 -0400

    Added test that generates the list for 1

---
+++ src/PrimeFactors/PrimeFactors.java
@@ -0,0 +1,12 @@
+package PrimeFactors;
+
+import java.util.List;
+
+public class PrimeFactors {
+
+	public static List<Integer> generate(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/PrimeFactors/PrimeFactorsTest.java
@@ -0,0 +1,22 @@
+package PrimeFactors;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class PrimeFactorsTest {
+
+	@Test
+	public void test1() {
+		assertEquals(list(), PrimeFactors.generate(1));
+	}
+
+	private List<Integer> list() {
+		// TODO Auto-generated method stub
+		return Arrays.asList();
+	}
+
+}
