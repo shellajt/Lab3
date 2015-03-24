f6ff2eb2afab6f0001795c0f45430d48cfc0e997
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-03-24 15:18:47 -0400

    Added test 7, which tests 9

--- src/PrimeFactors/PrimeFactorsTest.java
+++ src/PrimeFactors/PrimeFactorsTest.java
@@ -46,4 +46,9 @@ public class PrimeFactorsTest {
 	public void testEight() throws Exception {
 		assertEquals(list(2,2,2), generate(8));
 	}
+	
+	@Test
+	public void testNine() throws Exception {
+		assertEquals(list(3,3), generate(9));
+	}
 }
