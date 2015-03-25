f901403c3e3d87d747c11af69df0cf40900c19e6
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-03-25 16:04:11 -0400

    Created test file and class for the prime number generator

---
+++ src/PrimeNumberGenerator/PrimeNumberGenerator.java
@@ -0,0 +1,10 @@
+package PrimeNumberGenerator;
+
+public class PrimeNumberGenerator {
+
+	public static Object generate(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/PrimeNumberGenerator/PrimeNumberGeneratorTest.java
@@ -0,0 +1,21 @@
+package PrimeNumberGenerator;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class PrimeNumberGeneratorTest {
+
+	private List<Integer> list(Integer...integers) {
+		return Arrays.asList(integers);
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
